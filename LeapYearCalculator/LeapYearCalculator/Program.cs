0bae2b3e2a47f5f1202f404c7fcc6a0d576db52c
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-10-10 16:47:57 -0700

    Problem 2 completed

---
+++ LeapYearCalculator/LeapYearCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeapYearCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a year to evaluate if it is a leap in the format of YYYY: ");
+            int year = int.Parse(Console.ReadLine());
+
+            string result = ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 400 == 0)) ? $"Year {year} is a leap year!" : $"Year {year} is a NOT leap year.";
+            
+            Console.WriteLine(result);
+        }
+    }
+}

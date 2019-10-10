e5710bf1a358d9bd1838786d5378a980ebfe55b9
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-10-10 13:34:51 -0700

    application successfully accepts valid user inputs and outputs an array

--- ArrayMaxResult/ArrayMaxResult/Program.cs
+++ ArrayMaxResult/ArrayMaxResult/Program.cs
@@ -6,7 +6,41 @@ namespace ArrayMaxResult
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int counter = 0;
+            int[] numbers = new int[5];
+            while (counter < 5)
+            {
+                Console.Clear();
+                Console.Write("Please choose a number between 1 to 10. Same number can be chosen multiple times. ({0}/5): ", counter);
+                string numberInput = Console.ReadLine();
+                if (int.TryParse(numberInput, out int number) && 0 < number && number < 10)
+                {
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (numbers[i] == 0)
+                        {
+                            numbers[i] = number;
+                            break;
+                        }
+                    }
+                    counter++;
+                    Console.Clear();
+                    Console.WriteLine("You have entered '{0}'. Press 'Enter' to choose another number. ({1}/5).", number, counter);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("That was an invalid entry. Press 'Enter' to try again.");
+                    Console.ReadLine();
+                }
+            }
+
+            // Source: https://www.codeproject.com/Questions/1202570/Why-this-source-code-giving-output-system-int-inst
+            Console.Clear();
+            Console.Write("You have entered [" + string.Join(", ", numbers) + "]. Please select a number from the list for a score: ");
+            int selectedNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(selectedNumber);
+            Console.ReadLine();
         }
     }
 }
--- README.md
+++ README.md
@@ -15,5 +15,15 @@ Criteria for the challenge includes
 ### Setup
 * This challenge will be completed via Visual Studio Community Version
 
+### Problem 1: Array Max Result
+
+### Problem 1 Instruction
+Create a Console application that does the following: 
+1. Ask user to input 5 numbers from 1 to 10
+2. Output an array containing the 5 numbers to the console
+3. Ask user to input 1 number from the selected 5 numbers
+3. Output the score of the number chosen by:
+  * # * frequency of the #
+
 ### Reflections and Comments
 * N/A
